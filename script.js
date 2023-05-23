88a340c3c5b9a9f7c24bbbbcabab58e2f868ded5
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-23 16:24:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let user1=document.getElementById("player1").value.trim();
+let user2=document.getElementById("player2").value.trim();
+function openGame(user1,user2){
+	if(user1===user2){
+		alert("both name can't be same);
+			  return;
+	}
+}
